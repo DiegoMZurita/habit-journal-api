1ae87f764c16d0e316c2cba979a4a13cc7a5fb37
DiegoMZurita <thedaired@gmail.com>
2025-12-26 19:16:39 -0300

    feat: iniciar migración a MongoDB reemplazando JPA por Spring Data MongoDB

--- src/main/java/com/habitjournal/habit_journal_api/model/Habit.java
+++ src/main/java/com/habitjournal/habit_journal_api/model/Habit.java
@@ -1,19 +1,19 @@
 package com.habitjournal.habit_journal_api.model;
 
-import jakarta.persistence.*;
+
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@Entity
-@Table(name = "habits")
+@Document(collection = "habits")
 public class Habit {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id
+    private String id;
     private String name;
 
-    @OneToMany(mappedBy = "habit", cascade = CascadeType.ALL, orphanRemoval = true)
     List<LogEntry> logEntries = new ArrayList<>();
 }
