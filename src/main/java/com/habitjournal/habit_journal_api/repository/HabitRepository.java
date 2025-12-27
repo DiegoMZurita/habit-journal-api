a4296a1a5d7d33e25b0b0a22e0e9803b6bc036c0
DiegoMZurita <thedaired@gmail.com>
2025-12-26 21:23:03 -0300

    feat: adaptar LogEntry y repositorios para persistencia en MongoDB con consultas personalizadas

--- src/main/java/com/habitjournal/habit_journal_api/repository/HabitRepository.java
+++ src/main/java/com/habitjournal/habit_journal_api/repository/HabitRepository.java
@@ -1,16 +1,12 @@
 package com.habitjournal.habit_journal_api.repository;
 
 import com.habitjournal.habit_journal_api.model.Habit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface HabitRepository extends JpaRepository<Habit, Long> {
-    @Query("SELECT DISTINCT h FROM Habit h JOIN FETCH h.logEntries")
-    List<Habit> findAllWithLogs();
+public interface HabitRepository extends MongoRepository<Habit, String>, CustomHabitRepository{
     Optional<Habit> findByName(String name);
 }
